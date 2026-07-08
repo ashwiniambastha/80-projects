549c6b28ae465b6a083f515299cfea002d3c906c
Ashwini <ashwiniambastha@gmail.com>
2026-07-08 10:39:25 +0530

    New project No 6 edited

--- 6. Drinks Quality Prediction System/Drinks-Quality-Prediction-System/setup.py
+++ 6. Drinks Quality Prediction System/Drinks-Quality-Prediction-System/setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "52-projects"
+AUTHOR_USER_NAME = "ashwiniambastha"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "ashwiniambastha@gmail.com"
 
 
 setuptools.setup(
