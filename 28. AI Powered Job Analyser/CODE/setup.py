eeda8c4ae55a26195654e816d305466969a1a6ef
Ashwini <ashwiniambastha@gmail.com>
2026-07-16 11:30:51 +0530

    setup.py updated

--- 28. AI Powered Job Analyser/CODE/setup.py
+++ 28. AI Powered Job Analyser/CODE/setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name= "AI Powered Job Analyzer",
     version= "1.0.0",
-    author= "Sudhanshu",
+    author= "Ashwini",
     packages=find_packages(),
     install_requires=requirements,
 )
